eae308384eae3a553c69a1edbf8cb5cf3aba3601
Ashok kumar <ashok.kumar@ashoks-MacBook-Pro-2.local>
2017-04-11 10:58:48 +0530

    Good job i could present with this error

--- app/src/main/java/com/packt/ashok/buddybuilddemo/MainActivity.java
+++ app/src/main/java/com/packt/ashok/buddybuilddemo/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a = 1;
+    int b = 0;
+    int result ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = a/b;
     }
 }
